012d42263f6c7f3ed3ad405ffad006eccebaa23e
EC2 Default User <ec2-user@ip-172-31-20-97.ec2.internal>
2022-05-04 16:11:18 +0000

    [Update]サイドバーにフォロー数・フォロワー数を表示

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]

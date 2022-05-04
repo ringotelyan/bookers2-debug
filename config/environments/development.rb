01c794338aca551887fe72f0fb25ff3ddb426030
EC2 Default User <ec2-user@ip-172-31-20-97.ec2.internal>
2022-05-04 06:39:30 +0000

    [Update]課題2解説

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "9e51e2204d494445ab9d4eb3ca5af25c.vfs.cloud9.ap-northeast-1.amazonaws.com"
   config.hosts << "9e51e2204d494445ab9d4eb3ca5af25c.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts << "10be304a0930465ea866ed15968238bf.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
-  get "home/about" => "homes#about"
-
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  get 'home/about' => 'homes#about'
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
 
 end

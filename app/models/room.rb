1fd053be1fd7a09069516c5dfcd52052a868abd0
EC2 Default User <ec2-user@ip-172-31-20-97.ec2.internal>
2022-05-10 09:47:23 +0000

    [Update]応用課題８a何故か解決した

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :user_rooms
   has_many :chats
+
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'chat/:id', to: 'chats#show', as: 'chat'
-  resources :chats, only: [:show, :create]
+
+
   get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
+  resources :chats, only: [:show, :create]
 
 end

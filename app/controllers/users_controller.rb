61c882115b9b1b18f4e5f235cc81ba6c04e6f53a
ringotelyan <sute.dh9999@gmail.com>
2022-07-03 15:43:03 +0000

    [Update]応用課題8b諦め

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,24 +8,8 @@ class UsersController < ApplicationController
     @book = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
-    @two_days_ago_book = @books.created_2days_ago
-    @three_days_ago_book = @books.created_3days_ago
-    @four_days_ago_book = @books.created_4days_ago
-    @five_days_ago_book = @books.created_5days_ago
-    @six_days_ago_book = @books.created_6days_ago
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
-    @book_by_day = @books.group_by_day(:created_at).size
-    # grounpdateのgroup_by_dayメソッドで投稿日(created_at)に基づくグルービンぐして個数計上
-    # => {Wed, 05 May 2021=>23, Thu, 06 May 2021=>20, Fri, 07 May 2021=>3, Sat, 08 May 2021=>0, Sun, 09 May 2021=>12, Mon, 10 May 2021=>2}
-
-    @chartlabels = @book_by_day.map(&:first).to_json.html_safe
-    # 投稿日付の配列を格納。文字列を含んでいると正しく表示されないので.to_json.html_safeでjsonに変換。
-    # => "[\"2021-05-05\",\"2021-05-06\",\"2021-05-07\",\"2021-05-08\",\"2021-05-09\",\"2021-05-10\"]"
-
-    @chartdatas = @book_by_day.map(&:second)
-    # 日別投稿数の配列を格納。
-    # => [23, 20, 3, 0, 12, 2]
   end
 
   def index

a4fb324588abbad7965b38521e2cee5552541409
ringotelyan <sute.dh9999@gmail.com>
2022-07-04 02:51:04 +0000

    [Update]応用課題7cモデル修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,16 @@
 class Group < ApplicationRecord
-  has_many :group_users
-  has_many :users, through: :group_users
+  has_one_attached :image
+  belongs_to :owner, class_nmae: 'User'
+  has_many :group_users, dependent: :destroy
 
   validates :name, presence: true
   validates :introduction, presence: true
-  attachment :image, destroy: false
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+
+  def is_owned_by?(user)
+    owner.id == user.id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy

705e0870b5d1546054053827a8fbdb3c485e9c57
ringotelyan <sute.dh9999@gmail.com>
2022-07-07 04:50:31 +0000

    [Update]応用課題9cコントローラ準備

--- app/controllers/exent_notices_controller.rb
+++ app/controllers/exent_notices_controller.rb
@@ -1,2 +1,26 @@
 class ExentNoticesController < ApplicationController
+
+  def new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    @group = group.find(params[:group_id])
+    @title = params[:title]
+    @body = params[:body]
+
+    event = {
+      :group => @group,
+      :title => @title,
+      :body => @body
+    }
+
+    EvenMailer.send_notifications_to_group(event)
+
+    render :sent
+  end
+
+  def sent
+    redirect_to group_path(params[:group_id])
+  end
 end

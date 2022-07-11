5c262c5366bf3fa16f553e6dfd4bda3f6545ecbf
ringotelyan <sute.dh9999@gmail.com>
2022-07-11 08:14:43 +0000

    [Update]元に戻した

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,26 +24,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user = current_user
-    if params[:book]
-      if @book.save(context: :publicize)
-        redirect_to book_path(@book), notice: "You have created book successfully."
-      else
-        @books = Book.all
-        render 'index'
-      end
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      if @book.update(is_draft: true)
-        redirect_to user_path(current_user), notice: "下書き保存しました"
-      else
-        render 'index'
-      end
+      @books = Book.all
+      render 'index'
     end
-    # if @book.save
-    #   redirect_to book_path(@book), notice: "You have created book successfully."
-    # else
-    #   @books = Book.all
-    #   render 'index'
-    # end
   end
 
   def edit
@@ -53,33 +39,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if params[:publicize_draft]
-      @book.attributes = book_params.merge(is_draft: false)
-      if @book.save(context: :publicize)
-        redirect_to book_path(@book.id), notice: "下書きを公開しました"
-      else
-        @book.is_draft = true
-        render :edit, alert: "下書きを公開できませんでした"
-      end
-    elsif params[:update_book]
-      @book.attributes = book_params
-      if @book.save(context: :publicize)
-        redirect_to book_path(@book.id), notice: "投稿を更新しました"
-      else
-        render :edit, alert: "更新に失敗しました"
-      end
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
-      if @book.update(book_params)
-        redirect_to book_path(@book.id), notice: "下書きを更新しました"
-      else
-        render :edit, alert: "更新できませんでした"
-      end
+      render "edit"
     end
-    # if @book.update(book_params)
-    #   redirect_to book_path(@book.id), notice: "You have updated book successfully."
-    # else
-    #   render "edit"
-    # end
   end
 
   def destroy
@@ -91,7 +55,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :is_draft)
+    params.require(:book).permit(:title, :body)
   end
 
   def ensure_correct_user
--- db/migrate/20220711070711_add_is_draft_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddIsDraftToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :is_draft, :boolean, null: false, default: false
-  end
-end
